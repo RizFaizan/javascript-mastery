0be478e883c7c9465d14023ea97ff34ea2b7b05f
Faizan Ahmad <162777329+RizFaizan@users.noreply.github.com>
2025-08-30 23:59:59 +0500

    All about console

---
+++ 01. Basics/script.js
@@ -0,0 +1,37 @@
+// Some console methods that oftenly used
+
+// First one -> log
+// console.log() - Prints general messages or values to the console for debugging.
+
+console.log("Hi, I'm Faizan Ahmad")
+
+
+
+// Second -> table
+// console.table() - Displays data in a neat table format, perfect for objects and arrays.
+
+console.table({
+    name: 'faizan',
+    class: '12th'
+})
+
+
+
+// Third -> warn
+// console.warn() - Shows yellow warning messages to alert about potential issues.
+
+console.warn("Warning! Don't copy anything.")
+
+
+
+// Fourth -> error
+// console.error() - Outputs red error messages to the console, used to highlight failures and problems in your code.
+
+console.error("An error is occuring.")
+
+
+
+// Fifth -> clear
+// console.clear() - Clears all previous messages from the console window.
+
+console.clear();

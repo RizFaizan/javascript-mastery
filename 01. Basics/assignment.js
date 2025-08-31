c3fd2c2ae0804634c2caa84ba551f5889f15551a
Faizan Ahmad <162777329+RizFaizan@users.noreply.github.com>
2025-08-31 23:17:01 +0500

    merged new script

---
+++ 01. Basics/assignment.js
@@ -0,0 +1,12 @@
+let name = "Faizan Ahmad";
+
+let whatDoYouWannaBecomeInYourLife = 'Programmer'
+
+let gender = 'male'
+
+let twitterHandle = 'faizan.ahmad18'
+
+console.log(name)
+console.log(whatDoYouWannaBecomeInYourLife)
+console.log(gender)
+console.log(twitterHandle)

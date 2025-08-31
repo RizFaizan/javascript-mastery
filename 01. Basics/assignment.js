3fa5f584c01c578e7c7a715642ac2407c0fcd28f
Faizan Ahmad <162777329+RizFaizan@users.noreply.github.com>
2025-08-31 23:25:42 +0500

    code quality

--- 01. Basics/assignment.js
+++ 01. Basics/assignment.js
@@ -6,7 +6,7 @@ let gender = 'male'
 
 let twitterHandle = 'faizan.ahmad18'
 
-console.log(name)
-console.log(whatDoYouWannaBecomeInYourLife)
-console.log(gender)
-console.log(twitterHandle)
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
